0cdb7d11f47af6d6cce3edc4f08d3d592c662866
Daniel Gonzalez <dyg2104@gmail.com>
2018-10-27 19:39:57 -0400

    remove duplicates

--- 2.1/ruby/part_1.rb
+++ 2.1/ruby/part_1.rb
@@ -21,7 +21,7 @@ class LinkedList
 			return
 		end
 
-		ptr = head
+		ptr = @head
 
 		until ptr.next.nil?
 			ptr = ptr.next
@@ -29,4 +29,64 @@ class LinkedList
 
 		ptr.next = Node.new(value)
 	end
+
+	def remove_duplicates()
+		ctr = {}
+
+		ptr = @head
+
+		until ptr.nil?
+			if ctr[ptr.value].nil?
+				ctr[ptr.value] = 1
+			else
+				ctr[ptr.value] += 1
+			end
+
+			ptr = ptr.next
+		end
+
+		ptr = @head
+
+		until ptr.next.nil?
+			value = ptr.next.value
+
+			if ctr[value] > 1
+				ptr.next = ptr.next.next
+				ctr[value] -= 1
+			else
+				ptr = ptr.next
+			end
+		end
+	end
+
+	def print_values()
+		ptr = @head
+
+		until ptr.nil?
+			puts ptr.value
+			ptr = ptr.next
+		end
+	end
 end
+
+list = LinkedList.new()
+list.insert(1)
+list.insert(2)
+list.insert(2)
+list.insert(2)
+list.insert(2)
+list.insert(3)
+list.insert(10)
+list.insert(2)
+list.insert(2)
+list.insert(4)
+list.insert(3)
+list.insert(25)
+list.insert(25)
+list.insert(25)
+list.insert(10)
+list.insert(100)
+list.print_values()
+puts "----"
+list.remove_duplicates()
+list.print_values()

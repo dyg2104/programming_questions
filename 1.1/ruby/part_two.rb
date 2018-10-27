1a6f83128a40f8701f16c3b2016f60d95e97946c
Daniel Gonzalez <dyg2104@gmail.com>
2018-10-27 12:31:46 -0400

    temp

--- 1.1/ruby/part_two.rb
+++ 1.1/ruby/part_two.rb
@@ -1,48 +1,91 @@
 # What if you cannot use additional data structures?
 
-def merge_sort(chrs)
+def merge(str)
+	merge_sort(str, 0, str.length)
+end
+
+# "dcbaab"
+
+	# 6 / 2 = 3
+
+	# 0-2             3-5
+
+
+	# "dcb"              "aab"
+
+    # 3 / 2 = 1          3 / 2 = 1
+
+    # 0-0, 1-2           0-0, 1-2
+
+
+    # "d",  "cb"         "a", "ab"
+
+
+         # 2 / 2 = 1
+
+         # 0-0 , 1-1
+
+         # 0, 2 
+
+def merge_sort(str, left, right)
 	# base case
-	return chrs if chrs.length <= 1
+	return if left == right
 
-	midpoint = chrs.length / 2
+	midpoint = (right - left) / 2
 
-	left = chrs[0..midpoint-1]
-	left_chrs = merge_sort(left)
+	# left = chrs[0..midpoint-1]
+	# left_chrs = merge_sort(left)
+	merge_sort(str, left, left + midpoint - 1)
+	
+	# right = chrs[midpoint..-1]
+	# right_chrs = merge_sort(right)
+	merge_sort(str, left + midpoint, right - 1)
 
-	right = chrs[midpoint..-1]
-	right_chrs = merge_sort(right)
+	left_ptr = left
+	right_ptr = left + midpoint
 
-	arr = []
+	"asle"
 
-	while left_chrs.length > 0 && right_chrs.length > 0
-		if left_chrs.first <= right_chrs.first
-			arr << left_chrs.shift
+	"as"  "le"
+
+	"as"   "el"
+
+	aesl
+
+	while left_ptr < midpoint && right_ptr < right
+		# puts "#{str} #{left} #{right}"
+		if str[left_ptr] <= str[right_ptr]
+			# str[left_ptr], str[right_ptr] = str[right_ptr], str[left_ptr]
+			left_ptr += 1
 		else
-			arr << right_chrs.shift
+			str[right_ptr], str[left_ptr] = str[left_ptr], str[right_ptr]
+			right_ptr += 1
 		end
 	end
 
-	while left_chrs.length > 0
-		arr << left_chrs.shift
-	end
+	# while left_chrs.length > 0
+	# 	arr << left_chrs.shift
+	# end
 
-	while right_chrs.length > 0
-		arr << right_chrs.shift
-	end
+	# while right_chrs.length > 0
+	# 	arr << right_chrs.shift
+	# end
 
-	arr
+	# arr
+
+	str
 end
 
 def is_unique(str)
-	str_sorted = merge_sort(str.split(""))
-	str_sorted_len = str_sorted.length
+	str_sorted = merge(str)
+	puts str_sorted
 
-	str_sorted_len.times do |x|
+	str_sorted.length.times do |x|
 		return false if str_sorted[x] == str_sorted[x+1]
 	end
 
 	return true
 end
 
-text = "dcba"
+text = "dcbaab"
 puts is_unique(text)
